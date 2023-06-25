f21e8b52db352f17791e823177dbec79d1167e28
DasDanya <klimachkov.danilka@mail.ru>
2023-06-25 21:38:42 +0300

    Initial commit

---
+++ src/main/java/ru/pin120/transystem/TransystemApplication.java
@@ -0,0 +1,13 @@
+package ru.pin120.transystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pin120/transystem/TransystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pin120.transystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
